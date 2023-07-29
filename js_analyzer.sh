4dae1c24564825f33c7f7cef1dff65649a63351d
Medusa <55945380+medusa0xf@users.noreply.github.com>
2023-07-29 20:28:12 +0530

    Create js_analyzer.sh

---
+++ js_analyzer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check if the input file argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
+fi
+
+# Input file containing JS URLs
+input_file="$1"
+
+# Output file to store the results
+output_file="output_results.txt"
+
+# Check if /usr/local/bin/extract.sh exists and is executable
+extract_sh="/usr/local/bin/extract.sh"
+if [ ! -x "$extract_sh" ]; then
+    echo "Error: /usr/local/bin/extract.sh script not found or not executable."
+    exit 1
+fi
+
+# Process each JS URL from the input file
+while read -r url; do
+    # Get the name of the JS file from the URL
+    js_file_name=$(basename "$url")
+
+    # Fetch the JS file using curl and pass the output to /usr/local/bin/extract.sh
+    result=$(curl -s "$url" | "$extract_sh")
+
+    # Write the JS file name and result to the output file
+    echo "JS File: $js_file_name" >> "$output_file"
+    echo "$result" >> "$output_file"
+
+    # Separator between each JS file's result
+    echo "--------------------" >> "$output_file"
+done < "$input_file"
+
+echo "Processing complete. Results written to $output_file."
